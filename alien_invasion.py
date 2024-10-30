8c5246c5c8544ec6afe1c3747dce87e4ab7f837b
Daryna Levkovych <thedale.11.11.11@gmail.com>
2024-10-30 21:00:57 +0200

    bullet improvements

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,10 +18,7 @@ class AlienInvasion:
         while True:
             gf.check_events(self.settings,screen,ship,bullets)
             ship.update()
-            bullets.update()
-            for bullet in bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    bullets.remove(bullet)
+            gf.update_bullets(bullets)
             print(len(bullets))
             
             gf.update_screen(self.settings,screen,ship,bullets)
--- game_functions.py
+++ game_functions.py
@@ -12,15 +12,19 @@ def check_events(settings,screen,ship,bullets):
             check_keyup_events(event,settings,screen,ship,bullets)
 
 def update_screen(settings,screen,ship,bullets):
-    screen.fill(settings.bg_color)
+    screen.fill(settings.bg_color) 
     for bullet in bullets.sprites():
         bullet.draw_bullet()
 
-
     ship.blitme()
 
-
     pygame.display.flip()
+
+def update_bullets(bullets):
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
 
 def check_keydown_events(event,settings,screen,ship,bullets):
     if event.key == pygame.K_RIGHT:
@@ -36,11 +40,12 @@ def check_keydown_events(event,settings,screen,ship,bullets):
     elif event.key == pygame.K_r:
         ship.tu_default_posityon()
     elif event.key == pygame.K_SPACE:
-        new_bullet1 = Bullet(settings,screen,ship)
-        bullets.add(new_bullet1)
+        if len(bullets)< settings.bullets_allowed:    
+            new_bullet1 = Bullet(settings,screen,ship)
+            bullets.add(new_bullet1)
 
-        new_bullet2 = Bullet(settings,screen,ship,centerx_factor=-15)
-        bullets.add(new_bullet2)
+            new_bullet2 = Bullet(settings,screen,ship,centerx_factor=-15)
+            bullets.add(new_bullet2)
 
 def check_keyup_events(event,settings,screen,ship,bullets):
     if event.key == pygame.K_RIGHT:
